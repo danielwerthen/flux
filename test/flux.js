24691f6808ffcbffa3a875ee6fc03ebf0c704993
Daniel Werthén <danielwerthen@gmail.com>
2012-11-05 15:21:03 +0100

    Networks and Tests

--- test/flux.js
+++ test/flux.js
@@ -1,10 +1,25 @@
 var mocha = require('mocha');
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var cap = require('chai-as-promised');
+
+var flux = require('../index')
+	, util = require('../lib/util')
+
+chai.use(cap);
 
 describe('Flux', function () {
-	it('should know its version', function () {
-		var flux = require('../index');
-		expect(flux.version).to.not.equal(undefined);
-		expect(flux.version).to.equal('0.0.0');
+	it('should connect properly', function (done) {
+		flux.start({ port: 3000 }, function (err) {
+		});
+		flux.start({ port: 3001, nodes: [ { port: 3000, host: 'localhost' } ] }, function (err) {
+		});
+	});
+	it('should hash properly', function () {
+		var str1 = 'Hash me up good'
+			, str2 = 'Test and see if it works'
+			, str3 = 'Hash me up good'
+		expect(util.hash(str1)).to.equal(util.hash(str3));
+		expect(util.hash(str1)).to.not.equal(util.hash(str2));
 	});
 });
