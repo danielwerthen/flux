3483f2a73bcf686122104bf17593f1f91aba5194
Daniel Werthén <danielwerthen@gmail.com>
2012-11-12 11:58:12 +0100

    Parse and validate

--- test/parserTest.js
+++ test/parserTest.js
@@ -6,3 +6,13 @@ var fs = p.signalify();
 for (var i in fs) {
 	fs[i].print();
 }
+var validate = p.validate(fs);
+if (!validate.passed) {
+	console.log('There is ' + validate.messages.length + ' errors present.');
+}
+else {
+	console.log('Validation passed');
+}
+for (var i in validate.messages) {
+	console.log(validate.messages[i].message);
+}
