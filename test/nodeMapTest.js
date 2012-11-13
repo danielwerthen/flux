edce3a89ecf6a9b5aee2913ee5f353937d56e23e
Daniel Werthén <danielwerthen@gmail.com>
2012-11-13 12:25:32 +0100

    Func

--- test/nodeMapTest.js
+++ test/nodeMapTest.js
@@ -1,6 +1,32 @@
 var NodeMap = require('../lib/nodeMap')
+	, Parser = require('../lib/parser')
+	, fs = require('fs')
+	, Executor = require('../lib/executor')
+	, NodeA = new Executor()
+	, NodeB = new Executor()
 	, map = new NodeMap();
 
-map.add('NodeA', {}, 'Added', 'Fig');
+function Add(a, b, callback) {
+	callback(a + b);
+}
 
-map.resolve(process.argv[2] || 'NodeAbbb');
+NodeA.add('Add', { params: ['a', 'b'], args: ['result'] }, Add);
+
+NodeA.add('Print', { params: ['result'] }, function (result, callback) {
+	console.log(result);
+	callback();
+});
+
+NodeB.add('Subtract', { params: ['a', 'b'], args: ['result'] }, function (a, b, callback) {
+	callback(a - b);
+});
+
+var p = new Parser(fs.readFileSync('./test/calculus.flu', 'utf-8'));
+var fs = p.signalify();
+
+console.dir(fs);
+
+/*map.add('NodeA', NodeA, 'Added', 'Fig');
+map.add('NodeB', NodeB, 'Added');
+
+console.dir(map.resolve(process.argv[2] || 'NodeA'));*/
