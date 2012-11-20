202ef6d8ddb47264e4a05ec3b8490ace7ddfb3f8
Daniel Werthén <danielwerthen@gmail.com>
2012-11-20 12:00:07 +0100

    Tcp

--- test/connector.js
+++ test/connector.js
@@ -4,18 +4,8 @@ var _ = require('underscore')
 
 var args = process.argv.splice(2);
 
-var cc = new Con(args[0] || 3000, 'NodeA', 'Adder', 'Finder');
+var cc = new Con(args[0] || 3000, args.length > 1 ? 'NodeB' : 'NodeA', 'Adder', 'Finder');
 cc.start();
-if (false && args[1]) {
-	var outbound = new ns.NsSocket();
-	outbound.data(['you', 'there'], function () {
-		outbound.send(['iam', 'here'], { iam: true, indeedHere: true });
-	});
-	outbound.connect(args[1], function () {
-		console.log('done');
-		console.log('done');
-	});
-}
 
 for (var i = 1; i < args.length; i++) {
 	cc.connect('localhost', args[i]);
