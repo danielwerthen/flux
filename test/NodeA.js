46b2185cf31e9c7fe69521f185fd9d2055684d91
Daniel Werthén <danielwerthen@gmail.com>
2012-12-11 21:41:35 +0100

    Test

--- test/NodeA.js
+++ test/NodeA.js
@@ -32,7 +32,7 @@ var signal = flux.addSignal(fs.readFileSync('./calculus2.flu', 'utf-8'));
 signal.start();
 
 flux.connect({ connections: [ { url: 'http://localhost:3001', global: false, netspace: 'home' } ] }, function (err) {
-		if (err) console.log('fail');
-		if (err) return console.dir(err);
-		return console.log('Flux is loaded');
+	if (err) console.log('fail');
+	if (err) return console.dir(err);
+	return console.log('Flux is loaded');
 });
